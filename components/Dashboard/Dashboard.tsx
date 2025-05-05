203e06a8dc4c03fbe18dcf7d949037b14c084940
Bakasa <baksajoshua09@gmail.com>
2025-05-05 11:23:42 +0300

    Adding the Grid to the dashboard

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Grid } from "./Grid";
 import { Topbar } from "../Topbar/Topbar";
 
 export const Dashboard = () => {
     return (
         <div className="bg-white rounded-lg pb-4 shadow h-[200vh]">
             <Topbar />
+            <Grid />
         </div>
     )
 }
---
+++ components/Dashboard/Grid.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { StatCards } from "./StatCards";
+import { ActivityGraph } from "./ActivityGraph";
+
+export const Grid = () => {
+    return (
+        <div className="px-4 grid gap-3 grid-cols-12">
+            <StatCards />
+
+            <ActivityGraph />
+        </div>
+    );
+}
